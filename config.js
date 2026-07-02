bbd82e0887684118c1dfa455c59c3e78ed489939
shayethinkingloop <jmshypko@gmail.com>
2026-07-02 12:26:32 -0400

    Update config.js
    added quinn support

--- config.js
+++ config.js
@@ -1,22 +1,37 @@
 /* ============================================================
 HIIII QUINN HIIIII WELCOME TO YOUR WEBSITE CONFIG
 
-the way this website works and you have total control 
+   how does this work?
+
+   all of your images are stored in a folder and all of the text is stored here
+   drawing & painting are two different sections, indicated by [] square brackets
    
-   HOW TO ADD A NEW ARTWORK:
-   1. Drop the image file into the "images/" folder
-   2. Add a new entry to the drawings or paintings array below
-   3. Save the file — that's it.
+   hOW TO ADD A NEW ARTWORK:
+   1. drop the image file into the "images/" folder
+   2. add a new entry to the drawings or paintings section
    
-   IMAGE FILE NAMING: use lowercase, no spaces.
-   Good: "red-vase-study.jpg"  Bad: "Red Vase Study.jpg"
+   { title: "Lorem Ipsum",             size: "23 x 12", file: "quinn-drawing-1.jpeg" },
+
+   make sure 
+      a. this is kept in between the square brackets of the specific section
+      b. the file name matches the file name in the folder section
+      c. keep all "" '' and , -- these are likely sources of error 
+
+   saving changes
+     1.  remember to keep a backup of all text u write in case it doesn't save
+      2. hit commit changes in the big green button 
+      3. wait 5 mins
+      6. and if the changes don't persist text me IMMEDIATELY 
+   
+   how to name files: use lowercase and dashes
+   good: "quantle-bing-bong.jpg"  bad: "Quantle Stepbrothers Dove Kick.jpg"
    ============================================================ */
 
 const SITE_CONFIG = {
 
   /* ---------- ARTIST INFO ---------- */
   artistName:  "Quinn Antle",
-  birthYear:   "b. 2003",
+  birthYear:   "b. 2003", 
   instagramUrl: "https://www.instagram.com/yourhandle", // or "" to hide the link
 
   /* ---------- CTA BUTTON ---------- */
@@ -32,7 +47,7 @@ const SITE_CONFIG = {
   
   yeah u look like it`,
 
-  contact: `boom #Ya
+  contact: `boom #Ya swag
   
   vertical money spread`,
 
